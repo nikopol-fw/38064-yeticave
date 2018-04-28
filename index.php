dc2b76feedc6c0d093346d4e7d3815d6c59340f8
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2018-04-28 23:16:28 +0300

    fixup! Добавляет веб-сценарии выгрузки списка категорий в футер и списка лотов

--- index.php
+++ index.php
@@ -8,40 +8,40 @@ $categories = ['Доски и лыжи', 'Крепления', 'Ботинки',
 
 $goods = [
   [
-    'Название' => '2014 Rossignol District Snowboard',
-    'Категория' => 'Доски и лыжи',
-    'Цена' => '10999',
-    'URL картинки' => 'img/lot-1.jpg',
+    'name' => '2014 Rossignol District Snowboard',
+    'category' => 'Доски и лыжи',
+    'price' => '10999',
+    'url' => 'img/lot-1.jpg',
   ],
   [
-    'Название' => 'DC Ply Mens 2016/2017 Snowboard',
-    'Категория' => 'Доски и лыжи',
-    'Цена' => '159999',
-    'URL картинки' => 'img/lot-2.jpg',
+    'name' => 'DC Ply Mens 2016/2017 Snowboard',
+    'category' => 'Доски и лыжи',
+    'price' => '159999',
+    'url' => 'img/lot-2.jpg',
   ],
   [
-    'Название' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-    'Категория' => 'Крепления',
-    'Цена' => '8000',
-    'URL картинки' => 'img/lot-3.jpg',
+    'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+    'category' => 'Крепления',
+    'price' => '8000',
+    'url' => 'img/lot-3.jpg',
   ],
   [
-    'Название' => 'Ботинки для сноуборда DC Mutiny Charocal',
-    'Категория' => 'Ботинки',
-    'Цена' => '10999',
-    'URL картинки' => 'img/lot-4.jpg',
+    'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+    'category' => 'Ботинки',
+    'price' => '10999',
+    'url' => 'img/lot-4.jpg',
   ],
   [
-    'Название' => 'Куртка для сноуборда DC Mutiny Charocal',
-    'Категория' => 'Одежда',
-    'Цена' => '7500',
-    'URL картинки' => 'img/lot-5.jpg',
+    'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+    'category' => 'Одежда',
+    'price' => '7500',
+    'url' => 'img/lot-5.jpg',
   ],
   [
-    'Название' => 'Маска Oakley Canopy',
-    'Категория' => 'Разное',
-    'Цена' => '5400',
-    'URL картинки' => 'img/lot-6.jpg',
+    'name' => 'Маска Oakley Canopy',
+    'category' => 'Разное',
+    'price' => '5400',
+    'url' => 'img/lot-6.jpg',
   ]
 ];
 ?>
@@ -122,15 +122,15 @@ $goods = [
     <? foreach ($goods as $key => $good) { ?>
       <li class="lots__item lot">
         <div class="lot__image">
-          <img src="<?= $good['URL картинки']; ?>" width="350" height="260" alt="<?= $good['Название']; ?>">
+          <img src="<?= $good['url']; ?>" width="350" height="260" alt="<?= $good['name']; ?>">
         </div>
         <div class="lot__info">
-          <span class="lot__category"><?= $good['Категория']; ?></span>
-          <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $good['Название']; ?></a></h3>
+          <span class="lot__category"><?= $good['category']; ?></span>
+          <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $good['name']; ?></a></h3>
           <div class="lot__state">
             <div class="lot__rate">
               <span class="lot__amount">Стартовая цена</span>
-              <span class="lot__cost"><?= $good['Цена']; ?><b class="rub">р</b></span>
+              <span class="lot__cost"><?= $good['price']; ?><b class="rub">р</b></span>
             </div>
             <div class="lot__timer timer">
 
