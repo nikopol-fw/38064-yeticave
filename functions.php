e0e150f397b162690a1924690a078b9744bc70ed
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2018-05-02 17:20:48 +0300

    fixup! Добавляет реализацию шаблонизации, буферизации и защиты от XSS

--- functions.php
+++ functions.php
@@ -13,12 +13,12 @@ function format_price($price) {
   return $price_formatted;
 }
 
-function renderTemplate($templateUrl, $templateData) {
+function renderTemplate($templatePath, $templateData) {
 	$content = '';
 
-	if (file_exists($templateUrl)) {
+	if (file_exists($templatePath)) {
 		ob_start();
-		require($templateUrl);
+		require($templatePath);
 		$content = ob_get_contents();
 		ob_end_clean();
 	}
